58dc9e556e29f604cb0dd63c1cae6433dabd7f0f
Florian Gareis <mail@zoker.me>
2016-03-29 15:06:03 +0200

    First try of karma testing (not fully working yet)

---
+++ karma.conf.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.dev.config.js');
+webpackConfig.entry = {};
+
+module.exports = function (config) {
+  config.set({
+    browsers: [ process.env.CONTINUOUS_INTEGRATION ? 'Firefox' : 'Chrome' ],
+    singleRun: true, //just run once by default
+    frameworks: [ 'mocha' ], //use the mocha test framework
+    files: [
+      // all files ending in "_test"
+      'test/*_test.js',
+      'test/**/*_test.js'
+      // each file acts as entry point for the webpack configuration
+    ],
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-mocha',
+      'karma-sourcemap-loader',
+      'karma-webpack',
+    ],
+    preprocessors: {
+      // add webpack as preprocessor
+      'test/*_test.js': ['webpack', 'sourcemap'],
+      'test/**/*_test.js': ['webpack', 'sourcemap']
+    },
+    reporters: [ 'dots' ], //report results in this format
+    webpack: { //kind of a copy of your webpack config
+      devtool: 'inline-source-map', //just do inline source maps instead of the default
+      module: {
+        loaders: [
+          { test: /\.js$/, loader: 'babel-loader' }
+        ]
+      }
+    },
+    webpackServer: {
+      noInfo: true //please don't spam the console when running in karma!
+    }
+  });
+};
